81e2c14e2098eb84d21cde13529935d9b0181d37
AlexKole113 <alexander.koledov@gmail.com>
2020-11-02 22:43:58 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,21 @@
-const express       = require("express");
-const serverless    = require("serverless-http");
-const app           = express();
-const router        = express.Router();
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
 
 
-app.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
-app.use('/.netlify/functions/api', router )
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello from Express.js!</h1>');
+    res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 
+app.use('/.netlify/functions/app', router);  // path must route to lambda
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
 module.exports.handler = serverless(app);
