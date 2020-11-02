1f0c36af83ff325c53e32bc958c075dd7d17ff36
AlexKole113 <alexander.koledov@gmail.com>
2020-11-03 00:05:40 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,12 +1,13 @@
 const express       = require('express');
 const serverless    = require('serverless-http');
+const path          = require('path');
 const app           = express();
 const router        = express.Router();
 const fs            = require('fs');
 
 
 
-let indexHtml = fs.readFileSync(`../index.html`).toString();
+let indexHtml = fs.readFileSync( path.join(__dirname, '../index.html') ).toString();
 
 
 
