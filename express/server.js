eb138985a5316ac09d66fa1062a97c7bf6a41596
AlexKole113 <alexander.koledov@gmail.com>
2020-11-03 00:07:31 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -5,13 +5,11 @@ const app           = express();
 const router        = express.Router();
 const fs            = require('fs');
 
-
 let indexHtml = fs.readFileSync( path.join(__dirname, '../index.html') ).toString();
 
 app.use(express.static('.'));
 app.use('/.netlify/functions/server', router);
 app.get('*', (req, res) => { res.send( indexHtml ) } );
-
 
 module.exports = app;
 module.exports.handler = serverless(app);
