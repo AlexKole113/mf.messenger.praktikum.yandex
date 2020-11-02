cede7f86113ce742d3edbe19912261f14fe0c570
AlexKole113 <alexander.koledov@gmail.com>
2020-11-03 00:20:24 +0100

    sprint_3

--- server.js
+++ server.js
@@ -10,8 +10,8 @@
 const app   = require('./express/server');
 const PORT  = 80;
 app.listen( PORT, () => console.log( `Local app listening on port ${PORT}!` ));
-//app.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
+app.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
 
 
-let indexHtml = fs.readFileSync(`${__dirname}/index.html`).toString();
-app.get('*', (req, res) => { res.send( indexHtml ) } );
+// let indexHtml = fs.readFileSync(`${__dirname}/index.html`).toString();
+// app.get('*', (req, res) => { res.send( indexHtml ) } );
