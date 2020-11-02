c7d8be26ec7c2c1b2ed002b44f59eb2f95276db3
AlexKole113 <alexander.koledov@gmail.com>
2020-11-02 23:19:37 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -3,13 +3,12 @@ const path          = require('path');
 const serverless    = require('serverless-http');
 const app           = express();
 
-app.use(express.static('.'));
 
 const router = express.Router();
+//app.use(express.static('.'));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
