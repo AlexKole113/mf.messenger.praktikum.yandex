2e3d8826bd49343afc409b736faee1cb35049355
AlexKole113 <alexander.koledov@gmail.com>
2020-11-03 00:10:33 +0100

    sprint_3

--- express/server.js
+++ express/server.js
@@ -5,11 +5,16 @@ const app           = express();
 const router        = express.Router();
 const fs            = require('fs');
 
-let indexHtml = fs.readFileSync( path.join(__dirname, '../index.html') ).toString();
+
+
+let indexHtml = fs.readFileSync(`/index.html` ).toString();
+
+
 
 app.use(express.static('.'));
 app.use('/.netlify/functions/server', router);
 app.get('*', (req, res) => { res.send( indexHtml ) } );
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
