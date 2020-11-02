f709d6a695708c479c030c8d20e272dea8e11046
AlexKole113 <alexander.koledov@gmail.com>
2020-11-02 20:14:09 +0100

    sprint_3

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
-const express    = require('express');
+const express = require('express');
 
+const app = express();
+//const PORT = 4000;
+const PORT = 80;
 
-const serverless = require('serverless-http');
-const app        = express();
-const router     = express.Router()
-
-
-// const app = express();
-const PORT = 3000;
-// //const PORT = 80;
-//
-// app.use(express.static('.'));
+app.use(express.static('.'));
 app.listen(PORT, function () {
     console.log(`80 порт это пор по умолчанию для http, а текущий - ${PORT}!`);
 });
-//
-// app.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
 
-
-router.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
-app.use('/.netlify/functions/api',router)
-
-module.exports.handler = serverless(app)
+app.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
