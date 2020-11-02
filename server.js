e1b452bae2c2f5e5046717295e82e70b4084fb10
AlexKole113 <alexander.koledov@gmail.com>
2020-11-02 19:55:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const router     = express.Router()
 
 
 // const app = express();
-// const PORT = 3000;
+const PORT = 3000;
 // //const PORT = 80;
 //
 // app.use(express.static('.'));
-// app.listen(PORT, function () {
-//     console.log(`80 порт это пор по умолчанию для http, а текущий - ${PORT}!`);
-// });
+app.listen(PORT, function () {
+    console.log(`80 порт это пор по умолчанию для http, а текущий - ${PORT}!`);
+});
 //
 // app.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
 
