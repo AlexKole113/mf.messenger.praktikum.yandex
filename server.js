0ee98f80170b4f976a36e6cb82ea762418247a64
AlexKole113 <alexander.koledov@gmail.com>
2020-11-02 23:10:37 +0100

    sprint_3

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-
-const app = express();
+//const express = require('express');
+//
+// const app = express();
 //const PORT = 4000;
 const PORT = 80;
+//
 
-app.use(express.static('.'));
-app.listen(PORT, function () {
-    console.log(`80 порт это пор по умолчанию для http, а текущий - ${PORT}!`);
-});
+// app.listen(PORT, function () {
+//     console.log(`80 порт это пор по умолчанию для http, а текущий - ${PORT}!`);
+// });
 
+
+const app = require('./express/server');
+
+app.listen( PORT, () => console.log('Local app listening on port 3000!'));
 app.get('*', (req, res) => { res.sendFile(`${__dirname}/index.html`) })
